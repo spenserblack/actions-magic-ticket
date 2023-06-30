3793882776fe14185efaaa6a7174b4a36466aed6
Spenser Black <spenserblack01@gmail.com>
2023-06-30 21:47:45 +0000

    Update helper

--- src/utils.ts
+++ src/utils.ts
@@ -4,16 +4,42 @@ import type {
   DiscussionCreatedEvent,
 } from "@octokit/webhooks-definitions/schema";
 
+export type EventType = "issue" | "pr" | "discussion";
+
+export class UnknownEventTypeError extends Error {
+  constructor(eventName: string) {
+    super(`Unknown event type: ${eventName}`);
+  }
+}
+
+export function getEventType<Raise extends boolean>(ghContext: any, raise: Raise): EventType | (Raise extends true ? never : null) {
+  const { eventName } = ghContext;
+
+  switch (eventName) {
+    case "issues":
+      return "issue";
+    case "pull_request":
+      return "pr";
+    case "discussion":
+      return "discussion";
+  }
+  if (raise) {
+    throw new UnknownEventTypeError(eventName);
+  }
+
+  return null as never;
+}
 /**
  * Gets the ID number of the issue, pull request, or discussion that triggered the workflow.
  */
 export function getNumber(ghContext: any): number | null {
-  const { payload: pl, eventName } = ghContext;
+  const eventType = getEventType(ghContext, false);
+  const { payload: pl } = ghContext;
 
-  switch (eventName) {
-    case "issues":
+  switch (eventType) {
+    case "issue":
       return (pl as IssuesOpenedEvent).issue.number;
-    case "pull_request":
+    case "pr":
       return (pl as PullRequestOpenedEvent).pull_request.number;
     case "discussion":
       return (pl as DiscussionCreatedEvent).discussion.number;
