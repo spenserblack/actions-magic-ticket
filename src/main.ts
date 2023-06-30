9db095145fb1f4c52151684662507d2c4ae3cb8e
Spenser Black <spenserblack01@gmail.com>
2023-06-30 21:52:19 +0000

    Post comment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import * as core from "@actions/core";
+import Api from "./api";
+import { getNumber } from "./utils";
+import { getOctokit, context } from "@actions/github";
+import { render } from "./message";
+
+const token = core.getInput("token", { required: true });
+const message = core.getInput("message", { required: true });
+const regex = new RegExp(core.getInput("regex", { required: true }));
 
 async function run(): Promise<void> {
-  core.error("Unimplemented");
+  const number = getNumber(context);
+  if (number === null) {
+    core.setFailed("Unable to determine issue, pull request, or discussion number.");
+    return;
+  }
+  const comment = render(message, context);
+  const octokit = getOctokit(token);
+  const api = new Api(octokit, context.repo.owner, context.repo.repo);
+  await api.postComment(context, number, comment);
 }
 
 run();
